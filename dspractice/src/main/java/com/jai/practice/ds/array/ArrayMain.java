9779a1e9fdec3ef36885ae8535fab8c087177325
Jai Krishan <jai.bhardwaj99@gmail.com>
2020-05-02 01:38:56 +0530

    DS Practice Java Project build with Gradle added

---
+++ dspractice/src/main/java/com/jai/practice/ds/array/ArrayMain.java
@@ -0,0 +1,9 @@
+package com.jai.practice.ds.array;
+
+public class ArrayMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ dspractice/src/main/java/com/jai/practice/ds/linkedlist/LinkedListMain.java
@@ -0,0 +1,9 @@
+package com.jai.practice.ds.linkedlist;
+
+public class LinkedListMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
