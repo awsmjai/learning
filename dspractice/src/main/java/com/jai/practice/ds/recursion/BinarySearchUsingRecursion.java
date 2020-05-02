103089add9a188d6a2464f8dd5c4ccd5dbe3b722
Jai Krishan <jai.bhardwaj99@gmail.com>
2020-05-03 00:57:19 +0530

    Recursion Programmes Added

---
+++ dspractice/src/main/java/com/jai/practice/ds/recursion/BinarySearchUsingRecursion.java
@@ -0,0 +1,42 @@
+package com.jai.practice.ds.recursion;
+
+public class BinarySearchUsingRecursion
+{
+	public static void main(String[] args)
+	{
+		int[] arr = {1, 2, 3, 4, 5, 6, 7};
+		final int number = findNumber(0, arr.length - 1, arr, 7);
+		System.out.println("number: " + number);
+	}
+
+	public static int findNumber(int start, int end, int[] arr, int number)
+	{
+		int mid = (start + end) / 2;
+		if( start == end )
+		{
+			if( arr[start] == number )
+			{
+				return start;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+
+		if( arr[mid] == number )
+		{
+			return mid;
+		}
+		else if( number < arr[mid] )
+		{
+			end = mid - 1;
+		}
+		else
+		{
+			start = mid + 1;
+		}
+		return findNumber(start, end, arr, number);
+	}
+}
+
---
+++ dspractice/src/main/java/com/jai/practice/ds/recursion/FindBiggestNumberUsingRecursion.java
@@ -0,0 +1,30 @@
+package com.jai.practice.ds.recursion;
+
+public class FindBiggestNumberUsingRecursion
+{
+	private static int biggestNumber = Integer.MIN_VALUE;
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		int[] arr = {5,2,17,8,1,12,0,3};
+		final int biggestNumberUsingRecursion = findBiggestNumberUsingRecursion(arr, arr.length - 1);
+		System.out.println("biggestNumber: "+ biggestNumberUsingRecursion);
+	}
+
+	public static int findBiggestNumberUsingRecursion(int[] arr, int n)
+	{
+		if( n < 0 )
+		{
+			return biggestNumber;
+		}
+
+		if( arr[n] > biggestNumber )
+		{
+			biggestNumber = arr[n];
+		}
+
+		return findBiggestNumberUsingRecursion(arr, n - 1);
+
+	}
+}
